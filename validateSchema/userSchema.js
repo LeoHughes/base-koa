756684200ef7179ad940702dbba24e4f56acb1c4
LeoHughes <zhihy123@qq.com>
2017-10-25 14:32:50 +0800

    add regex class

--- validateSchema/userSchema.js
+++ validateSchema/userSchema.js
@@ -1,4 +1,5 @@
 const joi = require('joi')
+const regex = require('../util/regex')
 
 /**
  * 用户接口规则校验类
@@ -7,21 +8,7 @@ const joi = require('joi')
  */
 class userChema {
 
-  constructor() {
-
-    //邮箱验证
-    this.email = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)/
-
-    //电话验证
-    this.phone = /(^[1][3][0-9]{9}$)|(^[1][4][0-9]{9}$)|(^[1][5][0-9]{9}$)|(^[1][7][0-9]{9}$)|(^[1][8][0-9]{9}$)/
-
-    //ip验证
-    this.ip = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])((\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){3}|(\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){5})$/
-
-    //中文验证
-    this.CN = /.*[\u4e00-\u9fa5]+.*$/
-
-  }
+  constructor() {}
 
 
   loginSchema() {
@@ -29,8 +16,8 @@ class userChema {
     return joi.object({
       userName: joi.string().min(6).max(15).required(),
       passWord: joi.string().min(6).max(15).required(),
-      email: joi.string().regex(this.email).required(),
-      name: joi.string().regex(this.CN)
+      email: joi.string().regex(regex.email).required(),
+      name: joi.string().regex(regex.CN)
     })
 
   }
